f5e14cadacc1c6a0f0902a1d13b78397702f218d
vsshk <v.s.shkodin@gmail.com>
2021-05-20 19:41:06 -0700

    ddd

--- src/views.js
+++ src/views.js
@@ -8,20 +8,19 @@ module.exports = app => {
   });
   app.get(`/login`, (req, res) => res.render('Login'));
 
-//  app.get(`/user-input`, (req, res) => {
+  app.get(`/user-input`, (req, res) => {
     /*
       User input vulnerability,
       if the user passes vulnerable javascipt code, its executed in user's browser
       ex: alert('hi')
     */
     let result = '';
-    try {
-//      result = require('util').inspect(eval(req.query.userInput));
+    try {      result = require('util').inspect(eval(req.query.userInput));
     } catch (ex) {
       console.error(ex);
     }
-//    res.render('UserInput', {
-//      userInput: req.query.userInput,
+    res.render('UserInput', {
+      userInput: req.query.userInput,
       result,
       date: new Date().toUTCString()
     });
